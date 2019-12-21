45b2d7e8ca542bf175b0211a2110febab4a69ffe
Stepan Golovanov <vilkelis@yandex.ru>
2019-12-21 20:28:37 +0500

    task2 fixed VERSION constant scope

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,7 +1,7 @@
-
-class Application < Rails::Application
-  #Application version
-  #see semver.org
-  VERSION = '0.1.0'.freeze
-
+module TestGuru
+  class Application < Rails::Application
+    #Application version
+    #see semver.org
+    VERSION = '0.1.0'.freeze
+  end
 end
