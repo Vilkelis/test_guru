6553df853dfe541c210a3a4e48762cf525fed340
Stepan Golovanov <vilkelis@yandex.ru>
2019-12-24 09:50:31 +0500

    rubocop changes

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 module TestGuru
   class Application < Rails::Application
-    #Application version
-    #see semver.org
-    VERSION = '0.1.0'.freeze
+    # Application version
+    # see semver.org
+    VERSION = '0.1.0'
   end
 end
