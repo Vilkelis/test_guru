18d2420230e66f69f6a10fe4a9e6e2fef70ff232
Stepan Golovanov <vilkelis@yandex.ru>
2020-01-14 22:06:32 +0500

    Task4 final fixes

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,10 +4,8 @@
 class Test < ApplicationRecord
   # Returns all tests who have specific category title
   def self.tests_for_category(category_title)
-    select(:title)
-      .joins('INNER JOIN categories ON categories.id = tests.category_id')
+    joins('INNER JOIN categories ON categories.id = tests.category_id')
       .where(categories: { title: category_title })
-      .order(title: :desc)
-      .map(&:title)
+      .order(title: :desc).pluck(:title)
   end
 end
