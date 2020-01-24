bb477c1b46cd6ad02a5012eee948382cd8aa6a9e
Stepan Golovanov <vilkelis@yandex.ru>
2020-01-24 13:40:57 +0500

    Task8 fix according review

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,11 +2,57 @@
 
 # Test controller
 class TestsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_object_not_found
+
+  before_action :find_test, only: %i[show edit update destroy]
+
   def index
     @tests = Test.all
   end
 
-  def show
+  def show; end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+    if @test.save
+      redirect_to test_path(@test)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @test.destroy!
+    redirect_to tests_path
+  end
+
+  def edit; end
+
+  def update
+    if @test.update(test_params)
+      redirect_to test_path(@test)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
+  end
+
+  def find_test
     @test = Test.find(params[:id])
+  end
+
+  def rescue_with_object_not_found(exception)
+    raise exception unless exception.model == 'Test'
+
+    render html: '<b>Error:</b> '.html_safe + exception.message
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tests, only: %i[index show] do
+  resources :tests do
     resources :questions, shallow: true, except: %i[index]
   end
 end
